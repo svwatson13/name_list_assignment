a7b9b4c764c4e068560c39270b106dfd44ca98e6
Sam Watson <SWatson@spartaglobal.com>
2019-12-10 09:30:10 +0000

    add exit to loop

--- names_run.py
+++ names_run.py
@@ -4,9 +4,14 @@ try:
     with open("names.txt", 'r') as file_to_read:
         lines = file_to_read.read().splitlines()
         for line in lines:
-            line = Newusers(line)
-            print(line.name)
-            line.person_info()
+            user_input = input(f'information on {line} ')
+            if user_input == 'exit':
+                break
+            else:
+                line = Newusers(line)
+                line.add_info(user_input)
+                print(line.name)
+                line.person_info()
 except FileNotFoundError as error:
     print('OMG I BIFFED IT')
 finally:
